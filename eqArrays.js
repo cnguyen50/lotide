903a6191a421bc64dd6b3b27932c8ebfc37c616e
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-11 01:48:16 +0000

    linted

--- eqArrays.js
+++ eqArrays.js
@@ -5,22 +5,18 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
 const eqArrays = function(array1, array2) {
-  let arrayEq = "";  
+  if (array1.length !== array2.length) {
+    return false;
+  } 
   for (let i = 0; i < array1.length; i++) {
-    for (let x = 0; x < array2.length; x++) {
-      if (array1[i] === array2[x]) {
-        arrayEq = true;
-      } else {
-        arrayEq = false;
-      }
-    }
-  } return arrayEq;
+    if (array1[i] !== array2[i]) {
+      return false;
+    } 
+  } return true; 
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); //true
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); //true
 eqArrays([1, 2, 3], [3, 2, 1]); // false
 eqArrays(["1", "2", "3"], ["1", "2", "3"]);//true
-
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
