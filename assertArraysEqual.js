f9c7135ff846a5a20e387c80a788221839ec46dd
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-11 01:44:21 +0000

    linted

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,30 +1,28 @@
 const eqArrays = function(array1, array2) {
-  let arrayEq = "";
+  if (array1.length !== array2.length) {
+    return false;
+  } 
   for (let i = 0; i < array1.length; i++) {
-    for (let x = 0; x < array2.length; x++) {
-      if (array1[i] === array2[x]) {
-        arrayEq = true;
-      } else {
-        arrayEq = false;
-      }
-    }
-  } return arrayEq;
+    if (array1[i] !== array2[i]) {
+      return false;
+    } 
+  } return true; 
 };
 
 const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
+  if (eqArrays(arr1, arr2) === true) {
     console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} === ${arr2}`);
-    return true;
+  
   } else {
     console.log(`🛑 🛑 🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-    return false;
+    
   }
 };
+
 
 assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 3]), false);
 
-//eqArrays([1, 2, 3], [1, 2, 3]), true
-//eqArrays([1, 2, 3], [3, 2, 3]), false
+
 
 //Functions that do not return values and instead print out messages to the console are more difficult to test using assertions. This is because we cannot simply compare their return value to an expected value/
