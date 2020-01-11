f1a52dfc6feaa4b07c0ef7909fc9d5181f0f7f5c
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-11 04:08:32 +0000

    /**/ testers

--- eqObject.js
+++ eqObject.js
@@ -37,9 +37,9 @@ const eqObjects = function(object1, object2) {
   } return results;
   
 };
-const cd = { c: "1", d: ["2", 3] };
+/*const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 console.log(assertEqual(eqObjects(cd, dc))) //true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(assertEqual(eqObjects(cd, cd2))) //false
+console.log(assertEqual(eqObjects(cd, cd2))) //false*/
