44a3876c3c77d0686b1cedada6e24daab39a59ed
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-11 02:15:20 +0000

    revised with assertArraysEquals

--- middle.js
+++ middle.js
@@ -1,25 +1,25 @@
 const eqArrays = function(array1, array2) {
-  let arrayEq = "";
+  if (array1.length !== array2.length) {
+    return false;
+  } 
   for (let i = 0; i < array1.length; i++) {
-    for (let x = 0; x < array2.length; x++) {
-      if (array1[i] === array2[x]) {
-        arrayEq = true;
-      } else {
-        arrayEq = false;
-      }
-    }
-  } return arrayEq;
+    if (array1[i] !== array2[i]) {
+      return false;
+    } 
+  } return true; 
 };
 
 const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
+  if (eqArrays(arr1, arr2) === true) {
     console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} === ${arr2}`);
-    return true;
+  
   } else {
     console.log(`🛑 🛑 🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-    return false;
+    
   }
 };
+
+
 
 const middle = function(numOfArray) {
   let output = [];
