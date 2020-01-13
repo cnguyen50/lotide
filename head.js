85944b7c1f3db5df1a49a8cdae95822e782a8e03
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-13 21:51:43 +0000

    MODULE

--- head.js
+++ head.js
@@ -14,6 +14,8 @@ const head = function(array) {
     return array[0];
 };
 
+
+
 // assertEqual(head([5,6,7]), 5);
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); removed test code
   
