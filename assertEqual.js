8db7a0643f9d042ad61f769c3f7c4131eb78b6db
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-13 21:39:00 +0000

    IMPORTED AND EXPORTED

--- assertEqual.js
+++ assertEqual.js
@@ -7,7 +7,7 @@ const assertEqual = function(actual, expected) {
 
 };
 
+module.exports = assertEqual;
 
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+ assertEqual("Lighthouse Labs", "Bootcamp");
+ assertEqual(1, 1);
