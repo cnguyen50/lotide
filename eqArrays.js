ba966536b19037d3a466dd0986fdacc9be0f08c1
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-15 09:24:49 +0000

    imported assertEqual,exported and refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -16,7 +11,5 @@ const eqArrays = function(array1, array2) {
   } return true; 
 };
 
+module.exports = eqArrays;
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); //true
-eqArrays([1, 2, 3], [3, 2, 1]); // false
-eqArrays(["1", "2", "3"], ["1", "2", "3"]);//true
