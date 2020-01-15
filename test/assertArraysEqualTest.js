3a420e10cfa0fe8203bc9490bb083e7e60236e05
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-15 09:42:53 +0000

    revised and refactored

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,4 +1,6 @@
-const assertArrays = require('../assertArraysEqual');
+const assertArraysEqual = require('../assertArraysEqual');
+
+
 
 ([1, 2, 3], [1, 2, 3]), true;
 ([1, 2, 3], [3, 2, 3]), false;
