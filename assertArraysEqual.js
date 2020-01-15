c13061be3d1c39f34c956be5d194ba87349d1eaa
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-15 09:32:31 +0000

    imported and exported

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } 
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    } 
-  } return true; 
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2) === true) {
@@ -20,9 +11,6 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 3]), false);
+module.exports = assertArraysEqual;
 
 
-
-//Functions that do not return values and instead print out messages to the console are more difficult to test using assertions. This is because we cannot simply compare their return value to an expected value/
