2f2b11d39c09189ba05e9d7fed7f051fe8506749
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-15 09:51:54 +0000

    imported and exported

--- head.js
+++ head.js
@@ -8,7 +8,7 @@ const head = function(array) {
     return array[0];
 };
 
-
+module.exports = head;
 
 // assertEqual(head([5,6,7]), 5);
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); removed test code
