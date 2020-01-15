81ada34827dd3c319981d355c82541321479a2e4
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-15 09:35:46 +0000

    imported,exported and refactored

--- middle.js
+++ middle.js
@@ -1,23 +1,7 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } 
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    } 
-  } return true; 
-};
+const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} === ${arr2}`);
-  
-  } else {
-    console.log(`🛑 🛑 🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-    
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
+
 
 
 
@@ -31,4 +15,6 @@ const middle = function(arr) {
   }
 };
 
-console.log(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
+
+//middle([1, 2, 3, 4, 5, 6]), [3, 4];
