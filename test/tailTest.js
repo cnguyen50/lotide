249b7be8db58065f26aad10771b6ee555f475b1d
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-15 10:46:35 +0000

    replace test code with describe/it

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,12 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe("#tail", () => {
+    it("slices(1) for ['Hello', 'Lighthouse', 'Labs']", () => {
+      assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+    });
+});
+//(["Hello", "Lighthouse", "Labs"]) replaced as tester
 
 
-tail(["Hello", "Lighthouse", "Labs"]);
 
-
-module.exports = tail;
