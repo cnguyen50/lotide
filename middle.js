66e367f31ab4bf53ab8a8ddc6ae252e9316cb857
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-16 04:55:53 +0000

    imported eqArrays and assertArraysEqual

--- middle.js
+++ middle.js
@@ -8,9 +8,9 @@ const assertArraysEqual = require('./assertArraysEqual');
 const middle = function(arr) {
   if (arr.length <= 2) {
     return [];
-  } else if (arr.length % 2 === 1) { // for Odd
+  } else if (arr.length % 2 === 1) { 
     return [arr[Math.floor(arr.length / 2)]];
-  } else { // for even
+  } else { 
     return [arr[arr.length / 2] - 1, arr[arr.length / 2]];
   }
 };
