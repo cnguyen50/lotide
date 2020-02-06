fd904ab9eb8b7bdfdabb6cc961630b3099e64ae9
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-06 22:41:42 +0000

    linted

--- findKey.js
+++ findKey.js
@@ -1,29 +1,30 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  
-  };
+let list =  {
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}; // x => x.stars === 2) // => "noma"
 
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
   
-const findKey = function (object,callback) {
-    for(let key of Object.keys(object)) {
-        if (callback(object[key])) {
-            return key;
-        }
-    }
 };
 
-// findKey(
-let list =  {
-    "Blue Hill": { stars: 1 },
-    "Akaleri": { stars: 3 },
-    "noma": { stars: 2 },
-    "elBulli": { stars: 3 },
-    "Ora": { stars: 2 },
-    "Akelarre": { stars: 3 }
-    } // x => x.stars === 2) // => "noma"
-    
-    assertEqual(findKey(list, x => x.stars === 2), "noma");
+  
+const findKey = function(object,callback) {
+  for (const key of Object.keys(object)) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+
+assertEqual(findKey(list, x => x.stars === 2), "noma");
