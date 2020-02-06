f802675bbb5325cf45a122658c969aaa1247eba3
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-06 22:30:32 +0000

    linted

--- middle.js
+++ middle.js
@@ -1,20 +1,23 @@
-const eqArrays = require('./eqArrays');
+// const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = require('./assertArraysEqual');
+// const assertArraysEqual = require('./assertArraysEqual');
 
 
 
 
 const middle = function(arr) {
+  const mid = arr.length / 2;
   if (arr.length <= 2) {
     return [];
+
   } else if (arr.length % 2 === 1) { 
-    return [arr[Math.floor(arr.length / 2)]];
+    return [arr[Math.floor(mid)]];
+
   } else { 
-    return [arr[arr.length / 2] - 1, arr[arr.length / 2]];
+    return [arr[mid] - 1, arr[mid]];
   }
 };
 
 module.exports = middle;
 
-//middle([1, 2, 3, 4, 5, 6]), [3, 4];
+console.log(middle([1, 2, 3, 4, 5, 6]))//outputs [3, 4];
--- takeUntil.js
+++ takeUntil.js
@@ -1,53 +1,47 @@
 const eqArrays = function(array1, array2) {
-    if (array1.length !== array2.length) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
       return false;
-    } 
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      } 
-    } return true; 
-  };
-  
-  const assertArraysEqual = function(arr1, arr2) {
-    if (eqArrays(arr1, arr2) === true) {
-      console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} === ${arr2}`);
-    
-    } else {
-      console.log(`🛑 🛑 🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-      
     }
-  };
+  } return true;
+};
+  
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} === ${arr2}`);  
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${arr1} !== ${arr2}`);   
+  }
+};
 
 
 
 const takeUntil = function(array, callback) {
-    const results = [];
-    for (const el of array) {
-        if (callback(el) === true) {
-        return results;
-        }
-        results.push(el);
+  const result = [];
+  for (const el of array) {
+    if (callback(el)){
+      return result;
+    } else {
+      result.push(el);
     }
-    return results;
+  }
+  return result;
 };
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+console.log(results1);// [ 1, 2, 5, 7, 2 ]
 
 console.log('---');
 
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-//expected output
-// [ 1, 2, 5, 7, 2 ]
-// ---
-// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+console.log(results2);// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
 
 
 assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
 assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
-//passed
